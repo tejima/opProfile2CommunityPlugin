e1e46fe55d3cb4524704b910687fbe20696e4145
手嶋守 <tejima@gmail.com>
2011-09-09 20:51:30 +0900

    fix #3.

--- lib/task/P2CTask.class.php
+++ lib/task/P2CTask.class.php
@@ -8,7 +8,7 @@ class P2CTask extends sfBaseTask
     mb_language("Japanese");
     mb_internal_encoding("utf-8");
     $this->namespace        = 'zuniv.us';
-    $this->name             = 'P2C';
+    $this->name             = 'Profile2Community';
     $this->aliases          = array('zu-p2c');
     $this->briefDescription = '';
   }
@@ -22,20 +22,17 @@ class P2CTask extends sfBaseTask
     foreach($po_list as $po){
       echo $po["Translation"]["ja_JP"]["value"];
       echo "\n";
-      $c_list = Doctrine_Query::create()->from("Community c")->where("c.name = ?",$po["Translation"]["ja_JP"]["value"])->fetchArray();
-      $c = @$c_list[0];
+      $c = Doctrine_Query::create()->from("Community c")->where("c.name = ?",$po["Translation"]["ja_JP"]["value"])->fetchOne(array(),Doctrine::HYDRATE_ARRAY);
       if(!$c){
-        echo "NO Community.name matched.\n";
+        echo "NO COMMUNITY.NAME MATCHED.\n";
       }else{
-        $mp_list = Doctrine_Query::create()->from("MemberProfile mp")->where("mp.profile_option_id = ?",$po["id"])->fetchArray();
-        
+        /*
         Doctrine_Query::create()->delete()->from("CommunityMember cm")->where("cm.community_id = ?",$c["id"])->execute();
         Doctrine_Query::create()->delete()->from("CommunityMemberPosition cmp")->where("cmp.community_id = ?",$c["id"])->execute();
+        echo "COMMUNITY MEMBER CLEARED.\n";
         
-        echo "Community member cleared.\n";
-
-
         //member_id = 1 is the owner. 
+       
         $cm = new CommunityMember();
         $cm->community_id = $c["id"];
         $cm->member_id = 1;
@@ -47,13 +44,29 @@ class P2CTask extends sfBaseTask
         $cmp->community_member_id = $cm->id;
         $cmp->name = "admin";
         $cmp->save();
-        
+        echo "COMMUNITY ADMIN ADDED.\n";
+        */
+
+        $mp_list = Doctrine_Query::create()->from("MemberProfile mp")->where("mp.profile_option_id = ?",$po["id"])->fetchArray();
+        //入っているべきでない人を追い出す
+        $notin = array("1");
         foreach($mp_list as $mp){
-          $member_id = $mp["member_id"];
-          $cm = new CommunityMember();
-          $cm->community_id = $c["id"];
-          $cm->member_id = $member_id;
-          $cm->save();
+          $notin[] = $mp["member_id"];
+        }
+        Doctrine_Query::create()->delete()->from("CommunityMember cm")->where("cm.community_id = ?",$c["id"])->andWhereNotIn("cm.member_id",$notin)->execute();
+        Doctrine_Query::create()->delete()->from("CommunityMemberPosition cmp")->where("cmp.community_id = ?",$c["id"])->andWhereNotIn("cmp.member_id",$notin)->execute();
+        foreach($mp_list as $mp){
+          $_cm = Doctrine_Query::create()->from("CommunityMember cm")->where("cm.member_id = ?",$mp["member_id"])->addWhere("cm.community_id = ?",$c["id"])->fetchOne(array(),Doctrine::HYDRATE_ARRAY);
+          if($_cm){
+            //skip
+            echo "  MEMBER_ID=${mp['member_id']} IS A MEMBER OF COMMUNITY_ID=${c['id']} SKIP.\n";
+          }else{
+            $cm = new CommunityMember();
+            $cm->community_id = $c["id"];
+            $cm->member_id = $mp["member_id"];
+            $cm->save();
+            echo "  MEMBER_ID=${mp['member_id']} ADDED TO COMMUNITY_ID=${c['id']}.\n";
+          }
         }        
       }
     }
